dae96619e8058bbce6d38479b8d082c3de811c78
pung8146 <pung8146@naver.com>
2021-05-05 23:29:55 +0900

    es6 최신문법의 사용과 정확한 기술방법을 공부해야될꺼같음

--- JS_backjun/10950.js
+++ JS_backjun/10950.js
@@ -7,6 +7,33 @@ const rl = readline.createInterface({
 const A = [];
 const B = [];
 
-rl.on("line", function (line){
-    for(let i = 0; i <=)
+rl.on("line", (line) => {
+    for(let i = 0; i < 10 ; i++;){
+        console.log(A[i]+B[i]);
+    }
 })
+
+// 위 내가해본것
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+    input.push(line.split(' '));
+}).on('close', () => {
+    const T = parseInt(input[0]);
+    for(let i=1; i<=T; i++){
+        const A = parseInt(input[i][0]);
+        const B = parseInt(input[i][1]);
+        console.log(A+B);
+    }
+
+    process.exit();
+})
+
+// 어렵당
---
+++ JS_backjun/8393.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process,stdout
+});
+
+const input = [];
+
+// rl.on('line', (line) => {
+//     input.push(line.split(' '));
+// }).on('close', () => {
+//     const T = parseInt(input[0]);
+//     for( i = 1; i <= 100000; i++){
+//         T += 1
+
+//         };
+//         console.log()
+// }).on('close', () => {
+//     process.exit();
+//   })
+
+// 위에 반복문 으로 하고싶었는데 안됨 ..
+
+rl.on('line', (line) => {
+    
+})
