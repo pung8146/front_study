b8f09f161454287e315ecb1012f2adeb62dc3a4f
pung8146 <pung8146@naver.com>
2021-05-08 12:03:31 +0900

    2741 번 문제에서 출력순서가 뒤집힘 // 왜 i> 0 부터일까?

---
+++ JS_backjun/2742.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let answer = '';
+
+for (let i = input; i > 0 ; i--) {
+    answer += i + '\n';
+}
+
+console.log(answer);
