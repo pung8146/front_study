1a851717e70a2a9c79200a23af9dea6d7e8ce3c3
pung8146 <pung8146@naver.com>
2021-06-10 17:35:02 +0900

    일차 배열 최솟값 최댓값 구하기

---
+++ JS_backjun2/10818.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let testCase = Number(input[0])
+
+for (let i = 1; i <= testCase; i++) {
+    let numbers = input[i].split(' ');
+}
--- JS_backjun2/1110.js
+++ JS_backjun2/1110.js
@@ -1,7 +1,6 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-
 // 1. 숫자를 임의로받음
 //  1.5 숫자가 10보다 작으면 맨앞에 0을 붙여줌
 // 2. 처음 그숫자를더한다 
@@ -10,17 +9,20 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 //  4.5 결과값이 원숫자와 같은가 => (정답 실행누적결과표시)
 // 5. 다를경우 3으로 반복
 
-let oriNum = 5;
-let arr = ["0"];
 
-let sumNum = '';
+// 1. 숫자 임의값을 받는다 oriNum 
+// 2. 처음 그 숫자를 더한다 나온결과값 sumNum
+// 3. oriNum 의 오른쪽숫자 sumNum 의 오른쪽 숫자를 구한다
+//  3.5. 이때 각각 10 보다 작은 숫자들은 왼쪽자리에 0 을 더해서 계산
+// 4. 오른쪽 숫자들을 더한다 (이때 나온 결과값이 oriNum 으로) 이때 실행값 count++
+//  4.5 sumNum = oriNum 다를경우 2 ~ 4 반복 
+// 5. 카운트 갯수 표기
 
-let count = 0;
 
+let oriNum = Number(input);
+let sumNum = ' ';
+let sumNum2 = ' ';
 
-
-if (0 < orinum < 10) {
-    arr["0"] + oriNum.split(' ');
-} else {
-    return;
+if (oriNum !== sumNum) {
+    Number(oriNum * 10)
 }
