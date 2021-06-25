b1734efd4d18f5e37d7d2437157561de856b04e7
pung8146 <pung8146@naver.com>
2021-06-25 12:34:56 +0900

    programmers lev1 phone

---
+++ programmers/runnig_game.js
@@ -0,0 +1,24 @@
+// 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+// 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+// 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+// 1.배열을 중복 되어있는걸 비교 한다.
+// 2.완주자(배열) - 참가자(배열) = 완주하지 못한자
+function solution(participant, completion) {
+    var answer = '';
+
+    const par = participant[i].sort();
+    const com = completion[i].sort();
+
+    function cal() {
+        if (participant[i] !== completion[i]) {
+            for (let i = 0; i < participant.length; i++) {
+
+            }
+        } else {
+            return answer;
+        }
+    }
+
+    return answer;
+}
