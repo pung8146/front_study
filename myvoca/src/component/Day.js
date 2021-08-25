e5ff12f864c75b30f328a17e1db3b27f4cb89706
pung8146 <pung8146@naver.com>
2021-08-25 14:50:11 +0900

    useEffect로 API가져오기

--- myvoca/src/component/Day.js
+++ myvoca/src/component/Day.js
@@ -1,20 +1,29 @@
-import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from './Word.js';
 
 export default function Day() {
     const day = useParams().day;
     // const { day } = useParams();
-    const wordList = dummy.words.filter(word => word.day === Number(day))
-    console.log(wordList)
-    
+    // const wordList = dummy.words.filter(word => word.day === Number(day))
+    const [words,setWords] = useState([]);    
+
+    useEffect(() => {
+        fetch(`https://localhost:3001/words?day=${day}`)
+        .then(res=> {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+        });
+    }, [day]);
 
     return (
     <>
      <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word => (
+                {words.map(word => (
                     <Word word={word} key={word.id}/>
                 ))}
             </tbody>
--- myvoca/src/component/DayList.js
+++ myvoca/src/component/DayList.js
@@ -1,17 +1,27 @@
-import dummy from "../db/data.json";
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function DayList() {
-    console.log(dummy);
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        fetch('https://localhost:3001/days')
+        .then(res=> {
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        });
+    }, []);
+    
     return <>
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>
                     Day {day.day}</Link>
                 </li>
-            ) )}
-            <li></li>
+            ))}
         </ul>
     </>
 }
