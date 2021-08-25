56e59a96ca3b0d712b614db045f76d5d10411c03
pung8146 <pung8146@naver.com>
2021-08-25 12:58:02 +0900

    JSON-server 가동까지

--- myvoca/src/App.js
+++ myvoca/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-import Hello from './component/Hello';
+import Header from './component/Header'
+import DayList from './component/DayList'
+import Day from './component/Day'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 function App() {
   return (
+  <BrowserRouter>
   <div className="App">  
-    <h3>props : properties</h3>
-    <Hello age={10}/>
-    <Hello age={20}/>
-    <Hello age={30}/>
+    <Header/>
+    <Switch>
+      <Route exact path="/">
+        <DayList />
+      </Route>
+      <Route path="/day/:day">
+         <Day/>
+      </Route>
+      <Route>
+        <EmptyPage></EmptyPage>
+      </Route>
+    </Switch>
   </div>
+  </BrowserRouter>
   );  
 }
 
---
+++ myvoca/src/component/Day.js
@@ -0,0 +1,24 @@
+import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
+import Word from './Word.js';
+
+export default function Day() {
+    const day = useParams().day;
+    // const { day } = useParams();
+    const wordList = dummy.words.filter(word => word.day === Number(day))
+    console.log(wordList)
+    
+
+    return (
+    <>
+     <h2>Day {day}</h2>
+        <table>
+            <tbody>
+                {wordList.map(word => (
+                    <Word word={word} key={word.id}/>
+                ))}
+            </tbody>
+        </table>
+    </>
+    )
+}
